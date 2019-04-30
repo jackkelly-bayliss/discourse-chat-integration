8e198b0779cc4c4314950feac1bde2ffe7837d59
Jack Kelly-Bayliss <jackkellybayliss@protonmail.com>
2019-04-30 16:31:30 +0100

    Added return statements

--- lib/discourse_chat/provider/discord/discord_provider.rb
+++ lib/discourse_chat/provider/discord/discord_provider.rb
@@ -66,7 +66,7 @@ module DiscourseChat
           }]
         }
 
-        message
+          return message
 
         end
 
@@ -93,7 +93,7 @@ module DiscourseChat
           }]
         }
 
-        message
+          return message
 
         end
 
